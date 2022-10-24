6a12da7ab243940985ed22e855130d29ec579df8
357907 <357907@664-161458-LAB.schools.mybps.org>
2022-10-24 10:50:29 -0400

    Professor, I just entered my code.

--- src/edu/wit/cs/comp1000/PA1a.java
+++ src/edu/wit/cs/comp1000/PA1a.java
@@ -1,11 +1,31 @@
 package edu.wit.cs.comp1000;
 
+import java.util.Scanner;
+
 // TODO: document this class
 public class PA1a {
 
 	// TODO: document this function
 	public static void main(String[] args) {
 		// TODO: write your code here
+		
+		Scanner input = new Scanner(System.in);
+		int Yards;
+		 System.out.printf("Enter Yards: "); 
+		 Yards = input.nextInt();
+		 
+		 
+		 int Feet;
+		 System.out.printf("Enter Feet: ");
+		 Feet= input.nextInt();
+		 int Inches;
+		 System.out.printf("Enter Inches: ");
+		 Inches= input.nextInt();
+		 int Sum;
+		 
+		
+		 Sum = (Yards*36 + Feet* 12 + Inches*1);
+		 System.out.printf( "The Sum is %d%n", Sum);
 	}
 
 }
